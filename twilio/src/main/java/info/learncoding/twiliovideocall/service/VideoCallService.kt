24bb6334c9d019f04ab234b4c453f7745fae7136
Anup <anupdey99@gmail.com>
2021-12-29 16:09:03 +0600

    intent filter updated

--- twilio/src/main/java/info/learncoding/twiliovideocall/service/VideoCallService.kt
+++ twilio/src/main/java/info/learncoding/twiliovideocall/service/VideoCallService.kt
@@ -508,7 +508,9 @@ class VideoCallService : LifecycleService() {
     private fun registerCallbackReceiver() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             callbackReceiver?.let {
-                registerReceiver(it, IntentFilter(TwilioSdk.ACTION_CALLBACK))
+                val intentFilter = IntentFilter(TwilioSdk.ACTION_CALLBACK)
+                intentFilter.addAction(TwilioSdk.ACTION_CALL_DATA)
+                registerReceiver(it, intentFilter)
                 registeredCallbackReceiver = true
             }
 
